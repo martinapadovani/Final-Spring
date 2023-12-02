431f9bd7b4208823e3d210ce952a508005404c1b
martinapadovani <martinapadovani@gmail.com>
2023-12-02 17:14:28 -0300

    cambio

--- src/main/java/com/example/demo/controlador/IndexControlador.java
+++ src/main/java/com/example/demo/controlador/IndexControlador.java
@@ -1,10 +1,11 @@
 package com.example.demo.controlador;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/")
 public class IndexControlador {
 
